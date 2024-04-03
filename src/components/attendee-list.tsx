efc9f2a6c98670da2c5fb68b5a6f1a4602829dd7
juniordbz <franciscojunior02@hotmail.com.br>
2024-04-02 23:41:28 -0300

    Add table components

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,6 +7,10 @@ import {
   Search,
 } from 'lucide-react'
 import { IconButton } from './icon-button'
+import { Table } from './table/table'
+import { TableHeader } from './table/table-header'
+import { TableCell } from './table/table-cell'
+import { TableRow } from './table/table-row'
 
 export function AttendeeList() {
   return (
@@ -23,105 +27,83 @@ export function AttendeeList() {
         </div>
       </div>
 
-      <div className=" border border-white/10 rounded-lg">
-        <table className="w-full ">
-          <thead>
-            <tr className="border-b border-white/10 ">
-              <th
-                style={{ width: 48 }}
-                className="text-left py-3 px-4 text-sm font-semibold"
-              >
-                <input
-                  type="checkbox"
-                  className="size-4 bg-black/20 rounded border-white/10 checked:bg-orange-400"
-                />
-              </th>
-              <th className="text-left py-3 px-4 text-sm font-semibold">
-                Código
-              </th>
-              <th className="text-left py-3 px-4 text-sm font-semibold">
-                Participantes
-              </th>
-              <th className="text-left py-3 px-4 text-sm font-semibold">
-                Data da inscrição
-              </th>
-              <th className="text-left py-3 px-4 text-sm font-semibold">
-                Data do check-in
-              </th>
-              <th
-                style={{ width: 64 }}
-                className="text-left py-3 px-4 text-sm font-semibold"
-              ></th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 10 }).map((_, index) => {
-              return (
-                <tr
-                  key={index}
-                  className="border-b border-white/10 hover:bg-white/5 "
-                >
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">
-                    <input
-                      type="checkbox"
-                      className="size-4 bg-black/20 rounded border-white/10"
-                    />
-                  </td>
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">955154</td>
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">
-                    <div className="flex flex-col gap-1">
-                      <span className="font-semibold text-white">
-                        Francisco Bezerra
-                      </span>
-                      <span>franciscojunior02@hotmail.com.br</span>
-                    </div>
-                  </td>
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">
-                    7 dias atrás
-                  </td>
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">
-                    3 dias atrás
-                  </td>
-                  <td className="text-zinc-300 py-3 px-4 text-sm ">
-                    <IconButton transparent>
-                      <MoreHorizontal className="size-4" />
-                    </IconButton>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td className="text-zinc-300 py-3 px-4 text-sm" colSpan={3}>
-                Motrando 10 de 228 itens
-              </td>
-              <td
-                className="text-zinc-300 py-3 px-4 text-sm text-right"
-                colSpan={3}
-              >
-                <div className="inline-flex items-center gap-8">
-                  <span>Página 1 de 23</span>
-                  <div className="flex gap-1.5">
-                    <IconButton>
-                      <ChevronsLeft className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronLeft className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronRight className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronsRight className="size-4" />
-                    </IconButton>
+      <Table>
+        <thead>
+          <tr className="border-b border-white/10 ">
+            <TableHeader
+              style={{ width: 48 }}
+              className="text-left py-3 px-4 text-sm font-semibold"
+            >
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border-white/10 checked:bg-orange-400"
+              />
+            </TableHeader>
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participantes</TableHeader>
+            <TableHeader>Data da inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader
+              style={{ width: 64 }}
+              className="text-left py-3 px-4 text-sm font-semibold"
+            ></TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: 10 }).map((_, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border-white/10"
+                  />
+                </TableCell>
+                <TableCell> 955154</TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">
+                      Francisco Bezerra
+                    </span>
+                    <span>franciscojunior02@hotmail.com.br</span>
                   </div>
+                </TableCell>
+                <TableCell>7 dias atrás</TableCell>
+                <TableCell>3 dias atrás</TableCell>
+                <TableCell>
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <TableCell colSpan={3}>Motrando 10 de 228 itens</TableCell>
+            <TableCell className="text-right" colSpan={3}>
+              <div className="inline-flex items-center gap-8">
+                <span>Página 1 de 23</span>
+                <div className="flex gap-1.5">
+                  <IconButton>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+              </div>
+            </TableCell>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   )
 }
